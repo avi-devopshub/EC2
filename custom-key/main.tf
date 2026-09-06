37935e8a7b3ed7cf53de4c26a2b60f4572358e24
Avinash-DevopsEr <avi.mk1998@gmail.com>
2026-09-06 15:45:16 +0530

    Add AWS key pair resource and update EC2 instance key_name reference

--- custom-key/main.tf
+++ custom-key/main.tf
@@ -1,7 +1,11 @@
+resource "aws_key_pair" "hyderabad"{
+    key_name = "hyderabad"
+    public_key = file("/root/.ssh/id_ed25519.pub")
+}
 resource "aws_instance" "ec2"{
     ami = "ami-0199ac7c9fbf9ed83"
     instance_type = "t3.micro"
-    key_name = file("/root/.ssh/id_ed25519.pub")
+    key_name = aws_key_pair.hyderabad.key_name
     tags = {
         Name = "custom-key"
     }
