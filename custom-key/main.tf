1d0b5fbc47941d1b6ad5455ea24739e6caf27895
Avinash-DevopsEr <avi.mk1998@gmail.com>
2026-09-06 15:55:01 +0530

    Update EC2 instance key_name reference to use custom_key

--- custom-key/main.tf
+++ custom-key/main.tf
@@ -5,7 +5,7 @@ resource "aws_key_pair" "custom_key"{
 resource "aws_instance" "ec2"{
     ami = "ami-0199ac7c9fbf9ed83"
     instance_type = "t3.micro"
-    key_name = aws_key_pair.hyderabad.key_name
+    key_name = aws_key_pair.custom_key.key_name
     tags = {
         Name = "custom-key"
     }
